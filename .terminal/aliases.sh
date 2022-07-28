23b63f3538f8d5990835544043c07c29da2b1c31
ahmedsat <ahmedsat303@gmail.com>
2022-07-28 21:59:25 +0200

    init

---
+++ .terminal/aliases.sh
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------
+# Copyright (C) 2022 AhmedSat
+#-------------------------------------------------------------------------
+
+# my aliases
+
+alias search='nala search'
+alias show="nala show"
+alias update='sudo nala update'
+alias upgrade='sudo nala upgrade '
+alias clean='sudo nala clean'
+alias install='sudo nala install'
+alias history='nala history'
+alias undo='sudo nala history'
+alias list='nala list --upgradable'
+alias purge='sudo nala purge'
+alias remove='sudo nala remove'
+alias autopurge='sudo nala autopurge'
+
+alias bash-reload='source ~/.bashrc' # reload .bashrc file
+
+# mobile server
+
+alias connect-to-mobile='ssh u0_a115@192.168.1.3 -p8022'
---
+++ symlinks.sh
@@ -0,0 +1,4 @@
+ln -sf $(pwd)/.emacs.d ~/.emacs.d
+ln -sf $(pwd)/.terminal ~/.terminal
+ln -sf $(pwd)/.bashrc ~/.bashrc
+ln -sf $(pwd)/.fancyprompts ~/.fancyprompts
