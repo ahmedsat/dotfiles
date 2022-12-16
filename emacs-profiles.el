2327596457766e53e7f822c022db6b4de141b5b1
ahmedsat <ahmedsat@ahmed.sat>
2022-12-16 13:40:30 +0200

    16-12-2022

---
+++ emacs-profiles.el
@@ -0,0 +1,3 @@
+(("doom" . ((user-emacs-directory . "~/.local/share/emacs/doom")))
+;; ("test" . ((user-emacs-directory . "~/.local/share/emacs/test")))
+ ("satmacs" . ((user-emacs-directory . "~/.local/share/emacs/satmacs"))))
