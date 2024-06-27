cf11e2d2bcbdbdea3bfe2e99c328f384eab25557
ahmedsat <ahmedsat303@gmail.com>
2024-06-27 21:17:24 +0300

    update

--- qtile/config.py
+++ qtile/config.py
@@ -13,7 +13,21 @@ from groups import *
 
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    layout.MonadTall(
+        align=layout.MonadTall._right,
+        border_focus="#ff5e00",
+        border_normal="#00a1ff",
+        border_width=1,
+        change_ratio=0.05,
+        margin=10,
+        max_ratio=0.75,
+        min_ratio=0.25,
+        min_secondary_size=85,
+        new_client_position='after_current',
+        ratio=0.75,
+        single_border_width=0,
+        single_margin=10,
+    ), 
     layout.Max(),
 ]
 
@@ -107,7 +121,7 @@ def currentLayouts():
         ),
         widget.CurrentLayoutIcon(
             custom_icon_paths=[os.path.expanduser(
-                "~/.config/qtile/icons")],
+                os.getenv("DOTFILES_PATH")+"/icons")],
             foreground=colors[2],
             background=colors[0],
             padding=0,
@@ -207,11 +221,17 @@ screens = [
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
-         start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
-         start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front()),
+    Drag(
+        [mod], "Button1", 
+        lazy.window.set_position_floating(),
+        start=lazy.window.get_position()),
+    Drag(
+        [mod], "Button3", 
+        lazy.window.set_size_floating(),
+        start=lazy.window.get_size()),
+    Click(
+        [mod], "Button2", 
+        lazy.window.bring_to_front()),
 ]
 
 dgroups_key_binder = None
@@ -219,15 +239,15 @@ dgroups_app_rules = []  # type: list
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
+
 floating_layout = layout.Floating(
     float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
         Match(wm_class="confirmreset"),  # gitk
         Match(wm_class="makebranch"),  # gitk
         Match(wm_class="maketag"),  # gitk
-	Match(wm_class="virtscreen"), #
-	Match(wm_class="Test"),  # testing app
+	    Match(wm_class="virtscreen"), #
+	    Match(wm_class="Test"),  # testing app
         Match(title="Test"),  # testing app
         Match(wm_class="examples"),  # testing app
         Match(wm_class="app"),  # testing app
@@ -241,28 +261,17 @@ floating_layout = layout.Floating(
     ]
 )
 auto_fullscreen = True
+
 focus_on_window_activation = "smart"
+
 reconfigure_screens = True
 
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
 auto_minimize = True
 
-# When using the Wayland backend, this can be used to configure input devices.
 wl_input_rules = None
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
 wmname = "LG3D"
-
 
 @hook.subscribe.startup_once
 def autostart():
-    #home = os.path.expanduser('~/.config//autostart.sh')
     subprocess.call(["autostart.sh"])
--- qtile/keys.py
+++ qtile/keys.py
@@ -3,66 +3,50 @@ from libqtile.lazy import lazy
 from vars import *
 
 terminalKeys = [
-    Key([], "t", lazy.group["scratchpad"].dropdown_toggle("term"), desc="Toggle main scratchpad"),
-    Key([], "d", lazy.group["scratchpad"].dropdown_toggle("debug"), desc="Toggle debug scratchpad"),
-    Key([], "l", lazy.group["scratchpad"].dropdown_toggle("log"), desc="Toggle log scratchpad"),
-    Key([], "s", lazy.group["scratchpad"].dropdown_toggle("scrcpy"), desc="Toggle log scratchpad"),
+    Key([], "t", lazy.group["scratchpad"].dropdown_toggle("term")),
+    Key([], "d", lazy.group["scratchpad"].dropdown_toggle("debug")),
+    Key([], "l", lazy.group["scratchpad"].dropdown_toggle("log")),
+    Key([], "s", lazy.group["scratchpad"].dropdown_toggle("scrcpy")),
 
 ]
 
 keys = [
-    # Switch between windows
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    Key(
-        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
-    ),
-    Key(
-        [mod, "shift"],
-        "l",
-        lazy.layout.shuffle_right(),
-        desc="Move window to the right",
-    ),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key(
-        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
-    ),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "c", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    # my custom key bindings
-    Key(["control", "shift"], "r", lazy.reload_config(), desc="Reload the config"),
-    Key([mod, "control", "shift"], "r", lazy.restart(), desc="Restart qtile"),
-    # Key([ctrl], "f", lazy.spawn(fileManger), desc="Lunch file manager"),
-    # Key([mod], "e", lazy.spawn("emacsclient -c"), desc="Lunch emacs client"),
-    # Key([mod], "m", lazy.spawn("emacs --with-profile=satmacs"), desc="Lunch emacs"),
-    # key([crtl,shift],"p", lazy.spawn("sudo systemctl poweroff"), desc="Shutdown PC"),
-    Key(
-        [alt],
-        "space",
-        lazy.widget["keyboardlayout"].next_keyboard(),
-        desc="Next keyboard layout.",
-    ),
+    Key([mod], "h", lazy.layout.left()),
+    Key([mod], "l", lazy.layout.right()),
+    Key([mod], "j", lazy.layout.down()),
+    Key([mod], "k", lazy.layout.up()),
+    
+    Key([mod, "shift"], "h", lazy.layout.swap_left()),
+    Key([mod, "shift"], "l", lazy.layout.swap_right()),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down()),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up()),
+    
+    Key([mod], "i", lazy.layout.grow()),
+    Key([mod], "m", lazy.layout.shrink()),
+    Key([mod], "n", lazy.layout.reset()),
+    Key([mod, "shift"], "n", lazy.layout.normalize()),
+    Key([mod], "o", lazy.layout.maximize()),
+    Key([mod, "shift"], "space", lazy.layout.flip()),
+    
 
-    Key([mod, "control", "shift"], "b", lazy.hide_show_bar("top"), desc="Toggle fullscreen"),
-    Key([mod], "r", lazy.spawn("dmenu_run"), desc="Spawn a dmenu"),
+    Key([mod],"Tab", lazy.layout.next()),
+    Key([mod],"space", lazy.next_layout()),
+    
+    Key([mod], "n", lazy.layout.normalize()),
+    Key([mod], "c", lazy.window.kill()),
+    Key([mod, "control"], "q", lazy.shutdown()),
+    
+    Key(["control", "shift"], "r", lazy.reload_config()),
+    Key([mod, "control", "shift"], "r", lazy.restart()),
+    Key([mod], "space",lazy.next_layout()),
+    Key([alt],"Tab",lazy.widget["keyboardlayout"].next_keyboard()),
+
+    Key([mod, "control", "shift"], "b", lazy.hide_show_bar("top")),
+    Key([mod], "r", lazy.spawn("dmenu_run_history")),
 
 
     # terminal keys
-    Key([mod], "Return",lazy.spawn(terminal),desc="Launch terminal"),
+    Key([mod], "Return",lazy.spawn(terminal)),
     KeyChord([mod], "t", terminalKeys),
 
 ]
